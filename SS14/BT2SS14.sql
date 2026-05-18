76705b5a169b585a4c08ac291e24e58753d45788
khoaa3093-gif <khoaa3093@gmail.com>
2026-05-18 09:19:44 +0700

    Update BT2SS14.sql

--- SS14/BT2SS14.sql
+++ SS14/BT2SS14.sql
@@ -4,35 +4,27 @@ DELIMITER //
 
 CREATE PROCEDURE TransferBed(
     IN p_patient_id INT,
-    IN p_old_bed_id INT,
     IN p_new_bed_id INT
 )
 BEGIN
-
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
-
+ 
         ROLLBACK;
-
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Giao dịch thất bại';
     END;
 
     START TRANSACTION;
 
     UPDATE Beds
-    SET status = 'Empty',
-        patient_id = NULL
-    WHERE bed_id = p_old_bed_id;
-    UPDATE Beds
-    SET status = 'Occupied',
-        patient_id = p_patient_id
-    WHERE bed_id = p_new_bed_id;
-    COMMIT;
+    SET patient_id = NULL
+    WHERE patient_id = p_patient_id;
 
+    UPDATE Beds
+    SET patient_id = p_patient_id
+    WHERE bed_id = p_new_bed_id;
+
+    COMMIT;
 END //
 
 DELIMITER ;
-
-CALL TransferBed(1,101,102);
-
-SELECT *
-FROM Beds;
