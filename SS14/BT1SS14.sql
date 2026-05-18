339e1ac8e516afb68817eaa04106cc9eb24957dd
khoaa3093-gif <khoaa3093@gmail.com>
2026-05-18 09:17:48 +0700

    Update BT1SS14.sql

--- SS14/BT1SS14.sql
+++ SS14/BT1SS14.sql
@@ -1,4 +1,4 @@
-CALL PayHospitalFee(1,200000);
+CALL PayHospitalFee(1,5000);
 
 SELECT * 
 FROM Wallets
@@ -12,41 +12,28 @@ DROP PROCEDURE IF EXISTS PayHospitalFee;
 
 DELIMITER //
 
-CREATE PROCEDURE PayHospitalFee(
-    IN p_patient_id INT,
-    IN p_amount DECIMAL(10,2)
-)
+CREATE PROCEDURE PayHospitalFee(IN p_patient_id INT, IN p_amount DECIMAL(18,2))
 BEGIN
-
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
-
+        -- Nếu có lỗi, hoàn tác toàn bộ
         ROLLBACK;
-
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Giao dịch thất bại: Đã hoàn tác!';
     END;
 
     START TRANSACTION;
 
-    UPDATE Wallets
-    SET balance = balance - p_amount
+    UPDATE Wallets 
+    SET balance = balance - p_amount 
     WHERE patient_id = p_patient_id;
-
-    UPDATE Patient_Invoices
-    SET debt_amount = debt_amount - p_amount
+    
+    UPDATE Patient_Invoices 
+    SET total_due = total_due - p_amount 
     WHERE patient_id = p_patient_id;
 
     COMMIT;
-
 END //
 
 DELIMITER ;
 
-CALL PayHospitalFee(1,200000);
-
-SELECT *
-FROM Wallets
-WHERE patient_id = 1;
-
-SELECT *
-FROM Patient_Invoices
-WHERE patient_id = 1;
+CALL PayHospitalFee(1,5000);
